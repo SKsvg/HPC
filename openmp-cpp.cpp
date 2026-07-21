f7fb772cf92fd68374e4175557c8f874013919d9
Shashinika Herath <157236950+SKsvg@users.noreply.github.com>
2026-07-21 11:53:18 +0100

    Add files via upload

--- openmp-cpp.cpp
+++ openmp-cpp.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <omp.h>
 int main(){
-  omp_set_num_threads(5 );
+  //omp_set_num_threads(5 );
   
   #pragma omp parallel
   {
         int nt = omp_get_num_threads();
         int myid = omp_get_thread_num();
-        std::cout<<"Hello DCS --- from " << myid << "of" <<nt<<'\n';
+        X++;
+        //std::cout<<"Hello DCS --- from " << myid << "of" <<nt<<'\n';
 	//printf("Hello DCS --- from %d of %d\n",myid,nt);
   }
+  printf("X=%d\n",X);
   return 0;
 }
--- openmp-pgm.c
+++ openmp-pgm.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <omp.h>
 int main(){
-  omp_set_num_threads(5 );
-  
+  omp_set_num_threads(4);
+  int x=0;
+  int tem[16]={0};
   #pragma omp parallel
   {
         int nt = omp_get_num_threads();
         int myid = omp_get_thread_num();
-	printf("Hello DCS --- from %d of %d\n",myid,nt);
+	//printf("Hello DCS --- from %d of %d\n",myid,nt);
+	
+	//for(int i=0;i<1000000;i++)
+	  //tem[myid]++;
+	  //x[myid]++; //update my id only
+        //printf("Thread %d ---> initial X = %d\n",myid,x[myid]);
+        //printf("X=%d\n",x);
+        
+        for(int i=0;i<4;i++)
+          
   }
+  /*int Y=0;
+  for(int i=0;i<8;i++)
+    Y+=tem[i];
+  printf("Y=%d\n",Y); //8000000 tread no=8
+  */
   return 0;
 }
